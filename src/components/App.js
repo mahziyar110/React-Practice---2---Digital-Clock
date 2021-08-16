2b2762fdf624aef2a59cb5c17d9ce867e6db63a1
mahziyar110 <mahziyar110@gmail.com>
2021-08-16 14:45:13 +0530

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
+function getCurrentTime() {
+    const time = new Date();
+    return time.toLocaleTimeString().toUpperCase();
+}
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { time: getCurrentTime() };
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.setState({ time: getCurrentTime() });
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
     render() {
 
-        return(
-            <>
-               
-            </>
+        return (
+            <div className="Clock">
+                <h3 id="time">{this.state.time}</h3>
+            </div>
         )
     }
 }
